6469f24b694392350aca0363a6370485beb1ea77
Rishabh Kumar <rishabh.kumar10101@icloud.com>
2025-04-22 22:00:28 +0530

    Landing Page done

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import Link from 'next/link';
 import { ArrowRight, Mouse } from 'lucide-react';
 
+
 export default function Home() {
   return (
     <main className="relative min-h-screen bg-black text-white overflow-hidden font-sans">
@@ -22,27 +23,27 @@ export default function Home() {
           <Link href="#">FAQ</Link>
           <Link href="#" className="flex items-center gap-1">Protection <ArrowRight size={14} /></Link>
         </nav>
-        <Link href="#" className="text-sm bg-white text-black px-4 py-1.5 rounded-full hover:bg-white/90 transition">Create Account</Link>
+        <Link href="/Marketlist" className="cursor-pointer text-sm bg-white text-black px-4 py-1.5 rounded-full hover:bg-white/90 transition">Create Account</Link>
       </header>
 
       {/* Hero Section */}
       <section className="flex flex-col justify-center items-center text-center min-h-screen px-6 z-10 relative">
-        <p className="text-xs uppercase tracking-widest text-white/50 mb-4">Unlock Your Assets Spark!</p>
+        <p className="text-xs uppercase tracking-widest text-white/50 mb-4">Unlock Your Investing Spark!</p>
         <h1 className="text-5xl md:text-6xl font-extrabold tracking-tight leading-tight mb-6">
-          One-click for <span className="text-white/60">Asset Defense</span>
+          One-click for <span className="text-white/60">AI Forcaster</span>
         </h1>
         <p className="text-white/60 max-w-xl text-base md:text-lg mb-10">
-          Dive into the art assets, where innovative blockchain tech meets financial expertise.
+          Dive into the World of Crypto, Let AI Agent help you with your Investing.
         </p>
 
         <div className="flex gap-4 mb-16">
-          <Link href="/market">
-            <button className="bg-white text-black px-6 py-3 rounded-full font-medium text-sm hover:bg-white/90 transition-all shadow-md">
+          <Link href="/Marketlist">
+            <button className="cursor-pointer bg-white text-black px-6 py-3 rounded-full font-medium text-sm hover:bg-white/90 transition-all shadow-md">
               Discover More
             </button>
           </Link>
           <Link href="#">
-            <button className="bg-white/10 border border-white/20 px-6 py-3 rounded-full font-medium text-sm hover:bg-white/5 transition-all">
+            <button className="cursor-pointer bg-white/10 border border-white/20 px-6 py-3 rounded-full font-medium text-sm hover:bg-white/5 transition-all">
               Open App
             </button>
           </Link>
